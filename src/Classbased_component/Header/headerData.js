12124614e8f4922e65f598fec1eccaa0c95a2b84
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-08-15 15:54:36 +0300

    push

---
+++ src/Classbased_component/Header/headerData.js
@@ -0,0 +1,16 @@
+import search from "../../commonResource/images/icons/search-icon-sm.png";
+import cart from "../../commonResource/images/icons/cart-sm.png";
+//let searchimg = search;
+//let cartimg = cart;
+const data = [
+  { url: "/mac", title: "mac" },
+  { url: "/iphone", title: "iPhone" },
+  { url: "/iPad", title: "iPad" },
+  { url: "/watch", title: "watch" },
+  { url: "/tv", title: "tv" },
+  { url: "/Music", title: "Music" },
+  { url: "/Support", title: "Support" },
+  { url: "#", img: `${search}` },
+  { url: "#", img: `${cart}` },
+];
+export default data;
